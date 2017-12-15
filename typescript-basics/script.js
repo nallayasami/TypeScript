a0eecc72a094cf5c39fea103d54655aa01f256a7
Nallayasami Kathiresan <nka@sigos.com>
2017-12-15 17:48:10 +0100

    End of section 4

--- typescript-basics/script.js
+++ typescript-basics/script.js
@@ -22,4 +22,60 @@ myFunciton = newFun;
 var myAge;
 //check types 
 var newNumber = 10;
-console.log(typeof newNumber);
+if (typeof newNumber == "number") {
+    console.log(typeof newNumber);
+}
+//never function
+function throwError() {
+    throw new Error();
+}
+// "strictNullChecks": true,              /* Enable strict null checks. */
+var nullTypeValue = null; //this cannot be changed since the type of the value is null... only valid when the flag is true
+var nullableValue = null;
+// Arrow function 
+var myFn = function () { return console.log('Hello world !'); };
+myFn();
+//Default parameter value 
+var myDefaultValFn = function (name) {
+    if (name === void 0) { name = 'Human'; }
+    return console.log('Hello world !' + name);
+};
+myDefaultValFn('NKA');
+//REST and SPREAD
+var numberAry = [10, 12, 31, 44];
+console.log('Actual', Math.max(10, 12, 31, 44));
+//SPREAD - array to list
+// Math.max.apply(Math, numberAry);
+console.log('Spread', Math.max.apply(Math, numberAry));
+//REST - list to array
+var makeArrayFn = function () {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return args;
+};
+console.log('Rest', makeArrayFn(1, 2, 3));
+var myAry = ['fName', 'lName'];
+var sampleArry = ['address'];
+myAry.push.apply(myAry, sampleArry);
+myAry.push.apply(myAry, sampleArry);
+console.log(myAry);
+//Destructuring Array
+// const myAry = ['fName', 'lName'];
+var val1 = myAry[0], val2 = myAry[1];
+console.log(val1, val2);
+//Destructuring Obj
+var myObj = {
+    'fName': 'NAL', 'lName': 'KAT'
+};
+var fName = myObj.fName, lName = myObj.lName;
+console.log(fName, lName);
+//with alias
+var firstName = myObj.fName, lastName = myObj.lName;
+console.log('with alias', firstName, lastName);
+//Template literals
+var myName = 'Nallai';
+var outputStr = "\n    Hello I'm " + myName + " !\n";
+console.log(outputStr);
+//# sourceMappingURL=script.js.map
--- typescript-basics/script.ts
+++ typescript-basics/script.ts
@@ -43,7 +43,55 @@ function throwError(): never {
     throw new Error();
 }
 
-
 // "strictNullChecks": true,              /* Enable strict null checks. */
 let nullTypeValue = null; //this cannot be changed since the type of the value is null... only valid when the flag is true
 let nullableValue: string | null = null;
+
+// Arrow function 
+
+let myFn = () => console.log('Hello world !');
+myFn();
+
+
+//Default parameter value 
+let myDefaultValFn = (name: string = 'Human') => console.log('Hello world !' + name);
+myDefaultValFn('NKA');
+
+//REST and SPREAD
+
+const numberAry = [10, 12, 31, 44];
+console.log('Actual', Math.max(10, 12, 31, 44));
+//SPREAD - array to list
+// Math.max.apply(Math, numberAry);
+console.log('Spread', Math.max(...numberAry));
+//REST - list to array
+let makeArrayFn = (...args: number[]) => args;
+console.log('Rest', makeArrayFn(1, 2, 3));
+
+const myAry = ['fName', 'lName'];
+const sampleArry = ['address'];
+myAry.push.apply(myAry, sampleArry);
+myAry.push(...sampleArry);
+console.log(myAry);
+
+//Destructuring Array
+// const myAry = ['fName', 'lName'];
+const [val1, val2] = myAry;
+console.log(val1, val2);
+//Destructuring Obj
+const myObj = {
+    'fName': 'NAL', 'lName': 'KAT'
+};
+const {fName, lName} = myObj;
+console.log(fName, lName);
+//with alias
+const {fName:firstName, lName:lastName} = myObj;
+console.log('with alias', firstName, lastName);
+
+//Template literals
+const myName ='Nallai';
+const outputStr = `
+    Hello I'm ${myName} !
+`;
+
+console.log(outputStr);
