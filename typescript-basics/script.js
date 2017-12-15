efbb7a6ee2483fc7af2100acfe47c63cd53ed356
Nallayasami Kathiresan <nka@sigos.com>
2017-12-15 15:12:09 +0100

    end of types

--- typescript-basics/script.js
+++ typescript-basics/script.js
@@ -1,5 +1,25 @@
 "use strict";
 console.log('Hello World');
+var Color;
+(function (Color) {
+    Color[Color["RED"] = 1] = "RED";
+    Color[Color["YELLOW"] = 2] = "YELLOW";
+    Color[Color["BLUE"] = 3] = "BLUE";
+})(Color || (Color = {}));
+console.log(Color);
 //Tuple is helpful  for fixed size well known concrete type structure
 var tuple = ['Hello', 'world', 12];
 console.log(tuple);
+//function type:
+// This function only allow the function which matches this signature
+var myFunciton;
+function newFun(val1, val2) {
+    return val1 * val2;
+}
+;
+myFunciton = newFun;
+//union type
+var myAge;
+//check types 
+var newNumber = 10;
+console.log(typeof newNumber);
--- typescript-basics/script.ts
+++ typescript-basics/script.ts
@@ -1,7 +1,49 @@
 
 console.log('Hello World');
 
-//Tuple is helpful  for fixed size well known concrete type structure
-let tuple:[string, string, number]=['Hello','world', 12];
+enum Color {
+    RED = 1,
+    YELLOW = 2,
+    BLUE = 3
+}
+console.log(Color);
 
+//Tuple is helpful  for fixed size well known concrete type structure
+let tuple: [string, string, number] = ['Hello', 'world', 12];
 console.log(tuple);
+
+//function type:
+// This function only allow the function which matches this signature
+let myFunciton: (val1: number, val2: number) => number;
+
+
+function newFun(val1: number, val2: number): number {
+    return val1 * val2;
+};
+
+myFunciton = newFun;
+
+
+//reusable type declaration
+type myFnType = (val1: String, Val2: String) => string;
+type myObjType = { name: String, getValue: myFnType, anotherFn: () => void };
+
+//union type
+let myAge: number | string;
+
+//check types 
+
+let newNumber = 10;
+if (typeof newNumber == "number") {
+    console.log(typeof newNumber);
+}
+
+//never function
+function throwError(): never {
+    throw new Error();
+}
+
+
+// "strictNullChecks": true,              /* Enable strict null checks. */
+let nullTypeValue = null; //this cannot be changed since the type of the value is null... only valid when the flag is true
+let nullableValue: string | null = null;
