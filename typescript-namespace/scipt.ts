8d07526f56d03ecdeabc4ea27bd37028536e4de7
Nallayasami Kathiresan <nka@sigos.com>
2018-01-03 19:42:30 +0100

    local name space

--- typescript-namespace/scipt.ts
+++ typescript-namespace/scipt.ts
@@ -10,4 +10,13 @@ console.log(myNewNameSpace.calcuateCircumferance(5));
 console.log(myNewNameSpace.calcuateTCircumferance(5));
 console.log(crl.calcuateCircumferance(5));
 
+
+namespace loacalNameSpace {
+    export const sayHello = () => {
+        console.log('Hello !');
+    }
+}
+
+loacalNameSpace.sayHello();
+
 //# sourceMappingURL=script.js.map
