f1aec899c2fa2d630297fc13f5338fcb9b93943d
Nallayasami Kathiresan <nka@sigos.com>
2018-01-09 17:42:15 +0100

    class implementation

--- typescript-interfaces/js/app.ts
+++ typescript-interfaces/js/app.ts
@@ -4,19 +4,39 @@ interface PersonInterface {
     name: string;
     // Optional
     age?: number;
+    getAge(): number;
     // Unknown prop name
     [propName: string]: any;
 }
 
-
-const person = {
-    name:"Sheldon",
-    age:40, //Optional
-    job:"physicist" //unknown property.
+const person: PersonInterface = {
+    name: "Sheldon",
+    age: 40, //Optional
+    job: "physicist", //unknown property.
+    getAge(): number {
+        return (this.age) ? this.age : 0;
+    }
 }
 
-function logPerson(person: PersonInterface){
+function logPerson(person: PersonInterface) {
     console.log(person);
 }
 
 logPerson(person);
+
+// without "Unknown prop name" declaration  this litral will throw error.
+// logPerson({ name: "Sheldon", unkonw: "", getAge() { return 0 } });
+
+class anotherPerson implements PersonInterface {
+
+    job: string = "Engineer";
+
+    constructor(public name: string,
+        public age: number) {
+    }
+    getAge(): number {
+        return (this.age) ? this.age : 0;
+    }
+}
+
+console.log(new anotherPerson("Nallai", 27));
